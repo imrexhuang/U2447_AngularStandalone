7c264382b1800e868a81a84bd0fcb3803cc1c567
imrexhuang <im@rex.tw>
2026-05-11 13:49:24 +0800

    import { Content1Component } from "../content1/content1.component"; import { Content2Component } from "../content2/content2.component";

--- u2447ws/projects/mod02c/src/app/home/home.component.ts
+++ u2447ws/projects/mod02c/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Content1Component } from "../content1/content1.component";
+import { Content2Component } from "../content2/content2.component";
 
 @Component({
   selector: 'uuu-home',
