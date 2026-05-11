558b9a4a7297891dc43c79d249c702d79166c41e
imrexhuang <im@rex.tw>
2026-05-11 15:43:51 +0800

    ng g c Notfound --project mod02c -t 補充教材 p.18

---
+++ u2447ws/projects/mod02c/src/app/notfound/notfound.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'uuu-notfound',
+  imports: [],
+  template: `
+    <p>
+      notfound works!
+    </p>
+  `,
+  styles: ``
+})
+export class NotfoundComponent {
+
+}
