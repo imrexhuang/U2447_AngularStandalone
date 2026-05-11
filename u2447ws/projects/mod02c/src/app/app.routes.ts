9faf8bcaf59539e701ab0aa57e6c819a1c04d38e
imrexhuang <im@rex.tw>
2026-05-11 15:21:46 +0800

    路由 講義 p.4-4 補充教材 p.16

--- u2447ws/projects/mod02c/src/app/app.routes.ts
+++ u2447ws/projects/mod02c/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+/**
+ * 路由
+講義 p.4-4
+補充教材 p.16
 
+ */
 export const routes: Routes = [
     {path:'home', component:HomeComponent, title:"首頁"},
     {path:'about', component:AboutComponent, title:"關於"},
