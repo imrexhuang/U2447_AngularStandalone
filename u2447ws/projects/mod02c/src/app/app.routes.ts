2b7de02493f431ec63d18ec4423eef7a7d65ed85
imrexhuang <im@rex.tw>
2026-05-11 15:45:58 +0800

    app.routes.ts增加找不到網頁Notfound的路由設定

--- u2447ws/projects/mod02c/src/app/app.routes.ts
+++ u2447ws/projects/mod02c/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
 /**
  * 路由
 講義 p.4-4
@@ -8,6 +10,8 @@ import { AboutComponent } from './about/about.component';
 
  */
 export const routes: Routes = [
+    {path:'', redirectTo:'home', pathMatch:'full'},
     {path:'home', component:HomeComponent, title:"首頁"},
     {path:'about', component:AboutComponent, title:"關於"},
+    {path:'**', component:NotfoundComponent}
 ];
