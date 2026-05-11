0a062be87cfc5873150bb070f9a997d612de935a
imrexhuang <im@rex.tw>
2026-05-11 16:32:12 +0800

    複製老師的基礎專案 \starter\mod05

--- u2447ws/projects/mod05/src/app/app.component.ts
+++ u2447ws/projects/mod05/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
-  title = 'mod05';
+  title = '綁定';
 }
