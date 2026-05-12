b6a8cb3d81862a5f2b1afb764e600e43157169a2
imrexhuang <im@rex.tw>
2026-05-12 09:51:01 +0800

    事件傳遞練習

--- u2447ws/projects/mod05/src/app/content2/content2.component.ts
+++ u2447ws/projects/mod05/src/app/content2/content2.component.ts
@@ -19,4 +19,18 @@ export class Content2Component {
       target:${event.target.tagName} 
       currentTarget:${event.currentTarget.tagName}`; 
   }
+
+    GoHome(event: any) {   
+      event.preventDefault();　
+      //event.stopPropagation();　//停止向上傳遞
+      event.stopImmediatePropagation(); //也會停止水平傳遞
+
+    console.log('event.target.tagName:'+event.target.tagName);// 瀏覽器按F12看log
+    console.log('event.currentTarget.tagName:'+event.currentTarget.tagName);
+  }
+
+  myonclick(event: any) {
+    console.log(`myonclick()你點擊了 ${event.target.tagName} 元素`);
+  }
+
 }
