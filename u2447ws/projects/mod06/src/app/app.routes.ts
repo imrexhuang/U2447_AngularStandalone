ee93f0ccfb09a5bbb102262276a47756055c67bf
imrexhuang <im@rex.tw>
2026-05-12 15:26:11 +0800

    第六章 使用PIPE LAB 07 (補充教材 P.42)

--- u2447ws/projects/mod06/src/app/app.routes.ts
+++ u2447ws/projects/mod06/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { Lab03Component } from './lab03/lab03.component';
 import { Lab04Component } from './lab04/lab04.component';
+import { Lab07Component } from './lab07/lab07.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch:'full' },
     { path: 'home', component: HomeComponent, title: '首頁'},  
     { path: 'lab03', component: Lab03Component, title: '內容投影'},  
     { path: 'lab04', component: Lab04Component, title: '生命週期'}, 
+    { path: 'lab07', component: Lab07Component, title: 'Pipe'},  
     { path: 'about', component: AboutComponent, title: '關於'}, 
     { path: '**', component:NotfoundComponent}
 ];
--- u2447ws/projects/mod06/src/app/lab07/lab07.component.ts
+++ u2447ws/projects/mod06/src/app/lab07/lab07.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule  } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lab07',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lab07.component.html',
   styles: ``
 })
 export class Lab07Component {
+  vDate = new Date();
+  vString = "This is a book";
+  vMoney = 10000;
+  vNumber = 27.72;
+  vNumber2 = 31.13;
+  vPercent = 10;
 
 }
