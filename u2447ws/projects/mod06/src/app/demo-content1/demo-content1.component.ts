7d1f81b216c5a0123c23b04745f3e11140e2aa42
imrexhuang <im@rex.tw>
2026-05-12 13:20:39 +0800

    ng g c demoContent1 --project mod06 -t 補充教材 p.35 第六章 內容投影

---
+++ u2447ws/projects/mod06/src/app/demo-content1/demo-content1.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-content1',
+  imports: [],
+  template: `
+    <p>
+      demo-content1 works!
+    </p>
+  `,
+  styles: ``
+})
+export class DemoContent1Component {
+
+}
