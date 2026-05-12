6161ffb394e075b348c47ad82524eac45b38edb5
imrexhuang <im@rex.tw>
2026-05-12 15:20:23 +0800

    ng g c lab07 --project mod06 第六章 使用PIPE LAB 07 (補充教材 P.42)

---
+++ u2447ws/projects/mod06/src/app/lab07/lab07.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab07',
+  imports: [],
+  templateUrl: './lab07.component.html',
+  styles: ``
+})
+export class Lab07Component {
+
+}
