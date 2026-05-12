b9d29d035644f64effd3d74aa21d822185fe51f4
imrexhuang <im@rex.tw>
2026-05-12 10:02:13 +0800

    靶心遊戲

--- u2447ws/projects/mod05/src/app/content2/content2.component.ts
+++ u2447ws/projects/mod05/src/app/content2/content2.component.ts
@@ -14,17 +14,32 @@ export class Content2Component {
     this.eventValue = `你輸入: ${input.value}`;    
   }
 
+  canExit = false;
   onmousemove(event: any) {
     this.eventValue=`    
       現在位置 x:${event.offsetX} y:${ event.offsetY} 
       target:${event.target.tagName} 
       currentTarget:${event.currentTarget.tagName}`; 
+
+      const centerX = 75;
+      const centerY = 75;
+      const targetRange = 10;
+
+      if (Math.abs(event.offsetX - centerX) <= targetRange 
+          && Math.abs(event.offsetY - centerY) <= targetRange) {
+        this.canExit = true;
+        console.log("canExit");
+      }     
   }
 
     GoHome(event: any) {   
-      event.preventDefault();　
+      if(!this.canExit){
+         alert("必須正中靶心才能離開此頁面");
+         event.preventDefault();
+      }
+      //event.preventDefault();　
       //event.stopPropagation();　//停止向上傳遞
-      event.stopImmediatePropagation(); //也會停止水平傳遞
+      //event.stopImmediatePropagation(); //也會停止水平傳遞
 
     console.log('event.target.tagName:'+event.target.tagName);// 瀏覽器按F12看log
     console.log('event.currentTarget.tagName:'+event.currentTarget.tagName);
