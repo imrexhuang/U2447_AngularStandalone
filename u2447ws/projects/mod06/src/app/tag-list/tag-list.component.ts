cb54de6d0b7924bf53ba7d9a27ef74a1524a56ec
imrexhuang <im@rex.tw>
2026-05-12 11:45:30 +0800

    ng g c tagList --project mod06 -t --selector tagList 補充教材 第六章 LAB02 P.33

---
+++ u2447ws/projects/mod06/src/app/tag-list/tag-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tagList',
+  imports: [],
+  template: `
+    <p>
+      tag-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class TagListComponent {
+
+}
