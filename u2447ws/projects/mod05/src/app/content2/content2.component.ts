e7208cea880f9fa77c6427b4d1623ed89cf15c54
imrexhuang <im@rex.tw>
2026-05-12 09:33:07 +0800

    事件傳遞練習 課本p.5-7 補充教材 lab-03  p.25

--- u2447ws/projects/mod05/src/app/content2/content2.component.ts
+++ u2447ws/projects/mod05/src/app/content2/content2.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'uuu-content2',
+  selector: 'app-content2',
   standalone: true,
   imports: [],
   templateUrl: './content2.component.html',
   styles: ``
 })
 export class Content2Component {
-
+  eventValue:string="";
+  oninput(event: Event) {
+    const input = event.target as HTMLInputElement
+    this.eventValue = `你輸入: ${input.value}`;    
+  }
+  onmousemove(event: any) {
+    this.eventValue=`    
+      現在位置 x:${event.offsetX} y:${ event.offsetY} 
+      target:${event.target.tagName} 
+      currentTarget:${event.currentTarget.tagName}`; 
+  }
 }
