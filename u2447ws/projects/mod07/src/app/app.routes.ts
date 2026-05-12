b035248c1ba3687adbdcb0affd20de561c32209c
imrexhuang <im@rex.tw>
2026-05-12 16:56:33 +0800

    增加路由

--- u2447ws/projects/mod07/src/app/app.routes.ts
+++ u2447ws/projects/mod07/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { NotfoundComponent } from './layout/notfound.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { Content1Component } from './content1/content1.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch:'full' },
     { path: 'home', component: HomeComponent, title: '首頁'},  
+    { path: 'content1', component: Content1Component, title: 'input() 與 output()'},  
     { path: 'about', component: AboutComponent, title: '關於'}, 
     { path: '**', component:NotfoundComponent}
 ];
