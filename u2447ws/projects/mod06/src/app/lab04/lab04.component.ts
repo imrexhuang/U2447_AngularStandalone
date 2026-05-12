19033e671214de10606aeccfd6a333b98c3319e3
imrexhuang <im@rex.tw>
2026-05-12 13:44:18 +0800

    ng g c lab04 --project mod06 第六章 生命週期 LAB 04 (補充教材 P.37)

---
+++ u2447ws/projects/mod06/src/app/lab04/lab04.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab04',
+  imports: [],
+  templateUrl: './lab04.component.html',
+  styles: ``
+})
+export class Lab04Component {
+
+}
