b3ffc7c79b44f584baae9572e7957d3928d74e04
imrexhuang <im@rex.tw>
2026-05-12 11:28:26 +0800

    第六章 LAB01 補充教材 P.31

--- u2447ws/projects/mod06/src/app/home/home.component.ts
+++ u2447ws/projects/mod06/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { TagButtonComponent } from "../tag-button/tag-button.component";
 
 @Component({
-  selector: 'uuu-home',
-  standalone: true,
-  imports: [],
+  selector: 'uuu-home', 
+  imports: [TagButtonComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
 export class HomeComponent {
-
+  ondelete(tag:string){
+    alert(`delete: ${tag}`);
+  }
 }
