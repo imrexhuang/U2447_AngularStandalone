661ffac2561303ef0fc06fca894c9fbbbf4221dc
imrexhuang <im@rex.tw>
2026-05-12 13:22:28 +0800

    ng g c lab03 --project mod06 第六章 內容投影 LAB 03 (補充教材 P.36)

---
+++ u2447ws/projects/mod06/src/app/lab03/lab03.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab03',
+  imports: [],
+  templateUrl: './lab03.component.html',
+  styles: ``
+})
+export class Lab03Component {
+
+}
