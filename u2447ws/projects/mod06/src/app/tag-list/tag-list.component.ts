ef882b5ba713b1a2d6433c94bf4a4df22ca0e5e4
imrexhuang <im@rex.tw>
2026-05-12 11:48:39 +0800

    第六章 LAB02 補充教材 P.33

--- u2447ws/projects/mod06/src/app/tag-list/tag-list.component.ts
+++ u2447ws/projects/mod06/src/app/tag-list/tag-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { TagButtonComponent } from "../tag-button/tag-button.component";
 
 @Component({
   selector: 'tagList',
-  imports: [],
+  imports: [TagButtonComponent],
   template: `
-    <p>
-      tag-list works!
-    </p>
+     @if (!readOnly) {
+      <input type="text" #insterTag 
+        (keyup.enter)="addTag(insterTag.value); insterTag.value='';" 
+        placeholder="Add a tag" />
+      }
+    <div style="display: flex;">
+        @for (item of tags; track $index) {        
+            <tagButton [tag]="item" [canDelete]="!readOnly" (delete)="deleteTag($event)"></tagButton>       
+        }
+    </div>
+
   `,
   styles: ``
 })
