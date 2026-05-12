9af2b910527bd689608694e97c8ed50ac723d022
imrexhuang <im@rex.tw>
2026-05-12 13:45:41 +0800

    增加路由設定

--- u2447ws/projects/mod06/src/app/app.routes.ts
+++ u2447ws/projects/mod06/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { NotfoundComponent } from './layout/notfound.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { Lab03Component } from './lab03/lab03.component';
+import { Lab04Component } from './lab04/lab04.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch:'full' },
     { path: 'home', component: HomeComponent, title: '首頁'},  
     { path: 'lab03', component: Lab03Component, title: '內容投影'},  
+    { path: 'lab04', component: Lab04Component, title: '生命週期'}, 
     { path: 'about', component: AboutComponent, title: '關於'}, 
     { path: '**', component:NotfoundComponent}
 ];
