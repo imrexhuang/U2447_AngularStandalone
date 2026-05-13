4559002b2a4f295146eb5618ea87a43004054859
imrexhuang <im@rex.tw>
2026-05-13 10:08:52 +0800

    修改home.component

--- u2447ws/projects/mod08/src/app/home/home.component.ts
+++ u2447ws/projects/mod08/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CourseComponent } from "../course/course.component";
 
 @Component({
   selector: 'uuu-home',
   standalone: true,
-  imports: [],
+  imports: [CourseComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
