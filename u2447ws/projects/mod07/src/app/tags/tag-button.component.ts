b889c7e4e9fd15602c2dd8363c0a71264890c0e6
imrexhuang <im@rex.tw>
2026-05-13 09:10:10 +0800

    把@Input() @Output()元件寫法改成Signal寫法,第七章
    @Input() @Output():
    課本 p.6-2
    
    Signal:
    課本 p.7-3

--- u2447ws/projects/mod07/src/app/tags/tag-button.component.ts
+++ u2447ws/projects/mod07/src/app/tags/tag-button.component.ts
@@ -5,8 +5,12 @@ import { booleanAttribute, Component, EventEmitter, input, output,  } from '@ang
   imports: [],
   template: `
      <div  style="width: auto; background-color: beige; border-radius: 10px; padding: 6px; margin: 2px;">
-      <label  >{{ tagName }}</label> 
-      @if (canDelete) {
+      <!-- <label  >{{ tagName }}</label> 非Signal用法   -->
+       <!-- Signal用法要加()   -->
+      <label  >{{ tagName() }}</label> 
+     <!-- @if (canDelete) {  非Signal用法 -->
+      <!-- Signal用法要加()   -->
+      @if (canDelete()) {
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"
           (click)="onDelete()" >
           <path fill="red" d="M14.142 14.142c.781.781.781 2.047 0 2.828-.39.39-.902.586-1.414.586s-1.024-.195-1.414-.586L10 16.414l-1.414 1.414c-.781.781-2.047.781-2.828 0-.781-.781-.781-2.047 0-2.828L7.586 14l-1.414-1.414c-.781-.781-.781-2.047 0-2.828.781-.781 2.047-.781 2.828 0L10 11.586l1.414-1.414c.781-.781 2.047-.781 2.828 0 .781.781.781 2.047 0 2.828L12.414 14l1.414 1.414z"/>
