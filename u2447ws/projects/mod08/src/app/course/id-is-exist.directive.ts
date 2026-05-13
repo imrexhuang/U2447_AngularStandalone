e3fe74b016915b7c6163c3924e187b3f436dbc24
imrexhuang <im@rex.tw>
2026-05-13 13:22:31 +0800

    implements Validator

--- u2447ws/projects/mod08/src/app/course/id-is-exist.directive.ts
+++ u2447ws/projects/mod08/src/app/course/id-is-exist.directive.ts
@@ -1,10 +1,43 @@
-import { Directive } from '@angular/core';
+import { booleanAttribute, Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { courses } from '../model/coursedb';
 
 @Directive({
-  selector: '[appIdIsExist]'
+  selector: '[IdIsExist]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: IdIsExistDirective,
+    multi: true
+  }]
 })
-export class IdIsExistDirective {
+export class IdIsExistDirective implements Validator {
 
+  @Input({alias: "IdIsExist", transform: booleanAttribute}) isNew: boolean = false;
   constructor() { }
+
+  CheckIDExist(id: string): boolean {
+    let db = courses;
+    let course = db.find((c) => c.courseId === id);
+    return (course != null || course != undefined)
+  }
+
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    if (!this.isNew)
+      return null;
+
+    let value = control.value;
+    let result = null;
+    if (this.CheckIDExist(value)) {
+      result = {
+        "IdIsExist": {
+          "actualValue": value,
+          "requiredValue": `課程編號 ${value} 已經存在，請重新編號`
+        }
+      }
+    }
+    return result;
+
+  }
 
 }
