adbb468de948433fdf5690081d141cf122168fd4
imrexhuang <im@rex.tw>
2026-05-14 11:15:04 +0800

    使用HttpClient連到目的端
    LAB 02 (補充教材 P.78)

--- u2447ws/projects/mod10/src/app/part2/conn-to-web-api.service.ts
+++ u2447ws/projects/mod10/src/app/part2/conn-to-web-api.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConnToWebAPIService {
-
+  http=inject(HttpClient);
+  rootUrl="http://localhost:5046/wf";
   constructor() { }
+
+  GetSummary(id:number){
+
+    //使用HttpClient連到目的端
+    this.http.get(`${this.rootUrl}/${id}`,{responseType:'text'})
+      .subscribe( (data)=>{
+        console.log(data);
+      });
+  }
 }
