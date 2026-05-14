47c18a9fa2a104ef7edad45f23866acabe9b2cda
imrexhuang <im@rex.tw>
2026-05-14 15:31:33 +0800

    需要重啟(因為有改angular.json) ng s mod11

--- u2447ws/projects/mod11/src/app/meeting-room-detail/meeting-room-detail.component.ts
+++ u2447ws/projects/mod11/src/app/meeting-room-detail/meeting-room-detail.component.ts
@@ -3,6 +3,8 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { MeetingRoom } from '../shared/meeting-room';
 import { MeetingRoomService } from '../shared/meeting-room.service';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-meeting-room-detail',
@@ -19,9 +21,20 @@ export class MeetingRoomDetailComponent {
     { value: 30, text: "30人" }, { value: 40, text: "40人" },
     { value: 50, text: "50人" }, { value: 100, text: "100人" },
     { value: 200, text: "200人" }];
+
+  toastr = inject(ToastrService); //npm install ngx-toastr@19
+
   ngOnInit() {
     this.resetForm();
+
+    //ngx-toastr
+    this.toastr.show("show", "Title");
+    this.toastr.success("success", "Title");
+    this.toastr.info("info", "Title");
+    this.toastr.warning("warning", "Title");
+    this.toastr.error("error", "Title");    
   }
+  
   resetForm(form?: NgForm) {
     if (form != null)
       form.form.reset();
