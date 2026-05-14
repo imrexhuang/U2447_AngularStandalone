387ee6f840b440ea755c5cc67548f4d70f83db8c
imrexhuang <im@rex.tw>
2026-05-14 14:04:46 +0800

    Angular存取NET Core WebAPI
    LAB 01 (補充教材 P.87)
    第十一章
    
    啟動
    ng s mod11

--- u2447ws/projects/mod11/src/app/meeting-room-list/meeting-room-list.component.ts
+++ u2447ws/projects/mod11/src/app/meeting-room-list/meeting-room-list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MeetingRoomService } from '../shared/meeting-room.service';
 
 @Component({
   selector: 'app-meeting-room-list',
-  standalone: true,
   imports: [],
   templateUrl: './meeting-room-list.component.html',
   styles: ``
 })
 export class MeetingRoomListComponent {
+ service = inject(MeetingRoomService);
+  constructor() {
+    this.service.getList();
+  }
 
 }
--- u2447ws/projects/mod11/src/app/shared/meeting-room.service.ts
+++ u2447ws/projects/mod11/src/app/shared/meeting-room.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { MeetingRoom } from './meeting-room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetingRoomService {
 
-  constructor() { }
+  rootUrl = 'http://localhost:5046/MeetingRooms';
+  public formData = new MeetingRoom();
+  public list = signal<MeetingRoom[]>([]);
+  public recordCount=computed(()=>this.list().length);
+  public totalSize=computed(()=>this.list().reduce((acc, mr)=>acc+mr.size, 0));
+  http= inject(HttpClient);
+  getList() {
+    this.http.get<MeetingRoom[]>(this.rootUrl)
+      .subscribe(
+        {
+          next: resp => this.list.set(resp),
+          error: error => console.log(error)
+        });
+  }
+ getByID(id: number) {
+    this.http.get<MeetingRoom>(this.rootUrl + `/${id}`)
+      .subscribe({
+        next: resp => this.formData = resp,
+        error: error => console.log(error)
+      });
+  }
+
 }
