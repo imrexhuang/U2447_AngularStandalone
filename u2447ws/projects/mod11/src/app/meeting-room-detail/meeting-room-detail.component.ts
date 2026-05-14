bcca85747b3e5ed4858a0d9c95e38b33e4044147
imrexhuang <im@rex.tw>
2026-05-14 13:41:49 +0800

    拿老師的starter樣板蓋過

---
+++ u2447ws/projects/mod11/src/app/meeting-room-detail/meeting-room-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-room-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './meeting-room-detail.component.html',
+  styles: ``
+})
+export class MeetingRoomDetailComponent {
+
+}
---
+++ u2447ws/projects/mod11/src/app/meeting-room-list/meeting-room-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-room-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './meeting-room-list.component.html',
+  styles: ``
+})
+export class MeetingRoomListComponent {
+
+}
