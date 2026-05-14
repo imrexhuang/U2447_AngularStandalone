8d1ea36d9e22426e54ea72bd7476b440539c784d
imrexhuang <im@rex.tw>
2026-05-14 11:07:04 +0800

    ng g c part2 --project mod10
    LAB 02 (補充教材 P.78)

---
+++ u2447ws/projects/mod10/src/app/part2/part2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-part2',
+  imports: [],
+  templateUrl: './part2.component.html',
+  styles: ``
+})
+export class Part2Component {
+
+}
